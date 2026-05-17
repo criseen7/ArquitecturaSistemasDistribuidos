08074579a404335eb39fa1d74b7df9cee21f8b2b
Cristofer Hernandez <criseen7@gmail.com>
2026-05-16 19:56:30 -0600

    Agregando proxys al cliente del RCP

--- ProcedimientosRemotos/cliente.py
+++ ProcedimientosRemotos/cliente.py
@@ -3,5 +3,10 @@ import xmlrpc.client
 proxy = xmlrpc.client.ServerProxy(
     "http://localhost:8000/"
 )
-print(proxy.potencia(2, 8))
-print(proxy.factorial(5))
+print("=== CLIENTE RPC ===")
+print("Suma:", proxy.sumar(10, 5))
+print("Resta:", proxy.restar(10, 5))
+print("Multiplicación:", proxy.multiplicar(10, 5))
+print("División:", proxy.dividir(10, 5))
+print("Potencia:", proxy.potencia(2, 8))
+print("Factorial:", proxy.factorial(5))
